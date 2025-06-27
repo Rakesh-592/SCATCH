58a913f64f3e2ecc30beafc330e824c08eb0f7e4
Rakesh-592 <rakeshkunapareddy333@gmail.com>
2025-06-27 23:02:37 +0530

    dunno tryin to deploy

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 const config = require("config");
+require("dotenv").config(); // Only needed locally
+
+let MONGO_URI;
+
+try {
+  MONGO_URI = config.get("MONGO_URI");
+} catch (err) {
+  // If config not found (e.g., in Render), use env variable
+  MONGO_URI = process.env.MONGO_URI;
+}
 
 mongoose
-  .connect(`${config.get("MONGO_URI")}/scatch`)
+  .connect(`${MONGO_URI}/scatch`)
   .then(function () {
     dbgr("connected");
   })

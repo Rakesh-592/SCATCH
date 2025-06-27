fecf66706e6f46581e061f6c2dba736fcafd72a0
Rakesh-592 <rakeshkunapareddy333@gmail.com>
2025-06-27 22:38:43 +0530

    redone changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,40 +1,14 @@
-// const mongoose = require("mongoose");
-// const dbgr = require("debug")("development:mongoose");
-// const config = require("config");
-
-// mongoose
-//   .connect(`${config.get("MONGO_URI")}/scatch`)
-//   .then(function () {
-//     dbgr("connected");
-//   })
-//   .catch(function (err) {
-//     dbgr(err);
-//   });
-
-// module.exports = mongoose.connection;
-
-//for render deployment
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
-require("dotenv").config(); // Load variables from .env
-
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-  console.error("❌ MONGO_URI not set in environment variables");
-  process.exit(1);
-}
+const config = require("config");
 
 mongoose
-  .connect(`${MONGO_URI}/scatch`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(`${config.get("MONGO_URI")}/scatch`)
   .then(function () {
-    dbgr("✅ MongoDB connected");
+    dbgr("connected");
   })
   .catch(function (err) {
-    dbgr("❌ MongoDB error: " + err.message);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
